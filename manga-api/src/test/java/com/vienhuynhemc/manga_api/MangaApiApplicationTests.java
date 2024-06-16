1c5f3a290f79b4d7e37d456fd62b4fed6c04cfd4
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-06-16 15:25:58 +0700

    Create manga-api module

---
+++ manga-api/src/test/java/com/vienhuynhemc/manga_api/MangaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vienhuynhemc.manga_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
